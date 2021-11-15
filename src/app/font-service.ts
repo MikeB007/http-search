6c5f38c93c4b7924c10c300c00ff0ff4f5d0d538
kogutc <kogutc@hotmail.com>
2021-11-15 17:17:00 -0500

    cleanup

--- src/app/font-service.ts
+++ src/app/font-service.ts
@@ -1,24 +1,15 @@
 import { Injectable, OnInit } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-
 @Injectable({
   providedIn: 'root'
 })
 export class FontService implements OnInit{
-
   private fontSize = new BehaviorSubject('myinline3');
   currentFontSize = this.fontSize.asObservable();
-
-
   constructor() { }
-
-
-
   changeFont(newFont: string) {
     this.fontSize.next(newFont);
   }
-
-
   ngOnInit(){
     this.changeFont("myinline2");
   }
